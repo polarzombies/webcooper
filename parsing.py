26501ac3e96eec327af43b5ad6d9a2753a186fcd
Alexander Bodian <bodiana@wit.edu>
2018-04-12 16:22:20 -0400

    started coding

--- parsing.py
+++ parsing.py
@@ -9,3 +9,9 @@ __version__ = '1.0'
 import io
 from bs4 import BeautifulSoup
 import urllib
+
+website_url = 'http://www.nightswithalicecooper.com/on-the-air/last-nights-music/page/'
+
+numOfplaylists = input("Enter the number of nights of music: ")
+numOfplaylists = int(numOfplaylists)
+
